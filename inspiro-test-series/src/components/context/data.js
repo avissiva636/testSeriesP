8649507b61ab55f45e655729255e814fdd5cb99b
Jagan26042000 <jagan26042000@gmail.com>
2024-02-11 00:28:00 +0530

    data page creation & prelims demo data structure added

---
+++ inspiro-test-series/src/components/context/data.js
@@ -0,0 +1,57 @@
+export const prelimsTest = [
+  {
+    paid: [
+      {
+        title: "KAS Prelims Unit Wise Test Series",
+        description: "Highly recommended test series for your exam preparation",
+        details:
+          "http://exam.inspiroias.in/administrator/uploads/All_Karnataka_KAS_2024_Prelims_Test_Series_Schedule_batch_53.jpg",
+        question: [
+          {
+            qno: 1,
+            title: "Geography Lesson 1",
+            description: "KAS subject wise prelims test series",
+            time: 120,
+            instruction: "Some instructions about test",
+            nQuestion: 100,
+          },
+        ],
+      },
+    ],
+    prelims: [
+      {
+        title: "KAS Prelims Unit Wise Test Series",
+        description: "Highly recommended test series for your exam preparation",
+        payHere: "link for course purchasing",
+        details:
+          "http://exam.inspiroias.in/administrator/uploads/All_Karnataka_KAS_2024_Prelims_Test_Series_Schedule_batch_53.jpg",
+        fee: 5000,
+      },
+      {
+        title: "IAS Prelims Test Series",
+        description: "Highly recommended for IAS preparation",
+        payHere: "link for course purchasing",
+        details:
+          "http://exam.inspiroias.in/administrator/uploads/All_Karnataka_KAS_2024_Prelims_Test_Series_Schedule_batch_53.jpg",
+        fee: 40000,
+      },
+    ],
+  },
+];
+
+export const viewPrelimsQuestionPaper = [
+  {
+    questions: [
+      {
+        qno: 1,
+        question: "What is KAS prelims?",
+        options: {
+          option1: "Karnataka",
+          option2: "Tamilnadu",
+          option3: "Kerala",
+          option4: "Andra",
+        },
+      },
+    ],
+  },
+];
