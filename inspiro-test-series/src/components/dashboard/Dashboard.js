c9988a45e19dc3e0a765bd99351db66eea20fc55
Jagan26042000 <jagan26042000@gmail.com>
2024-02-11 23:16:02 +0530

    Dashboard setup

---
+++ inspiro-test-series/src/components/dashboard/Dashboard.js
@@ -0,0 +1,8 @@
+import Homepage from "../homepage/Homepage";
+const Dashboard = () => {
+    console.log("Reached dashboard");
+    return(
+        <duv><Homepage /></duv>
+    )
+}
+export default Dashboard;
