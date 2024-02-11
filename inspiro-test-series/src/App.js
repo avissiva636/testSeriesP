858c2fbdc983fa3820473a9a2cad641cfa03c1ba
Jagan26042000 <jagan26042000@gmail.com>
2024-02-11 23:03:06 +0530

    Route setup

--- inspiro-test-series/src/App.js
+++ inspiro-test-series/src/App.js
@@ -1,9 +1,22 @@
+import { InspiroContext } from "./components/context/InspiroContext";
+import Prelimspage from "./components/prelimsTestSeries/Prelimspage";
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+// import Explorer from "./components/homepage/Explorer";
 import Homepage from "./components/homepage/Homepage";
+import Dashboard from "./components/dashboard/Dashboard";
 
 function App() {
   return (
     <div>
-      <Homepage />
+      <Router>
+        <InspiroContext>
+          <Routes>
+            <Route path="/" element={<Homepage />} />
+            <Route path="Dashboard" element={<Dashboard />}/>
+            <Route path="/PrelimsTestStore" element={<Prelimspage />} />
+          </Routes>
+        </InspiroContext>
+      </Router>
     </div>
   );
 }
