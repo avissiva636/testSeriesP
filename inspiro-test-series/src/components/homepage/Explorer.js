a48e043c9642eb8c4fb8b11ab466d76009fc9135
Jagan26042000 <jagan26042000@gmail.com>
2024-02-11 23:18:35 +0530

    explorer navigation setup & Listitem custom creation

--- inspiro-test-series/src/components/homepage/Explorer.js
+++ inspiro-test-series/src/components/homepage/Explorer.js
@@ -1,4 +1,4 @@
-import { Box, Button, ListItem, Stack, Typography } from "@mui/material";
+import { ListItem, Stack } from "@mui/material";
 import ImportantDevicesIcon from '@mui/icons-material/ImportantDevices';
 import ListAltIcon from '@mui/icons-material/ListAlt';
 import FileCopyIcon from '@mui/icons-material/FileCopy';
@@ -8,23 +8,30 @@ import TrendingUpIcon from '@mui/icons-material/TrendingUp';
 import ArchiveIcon from '@mui/icons-material/Archive';
 import SettingsIcon from '@mui/icons-material/Settings';
 import Welcomepage from "./Welcomepage";
+import Customlistitem from "./Customlistitem";
+import { useNavigate } from "react-router-dom";
+
 
 
 const Explorer = () => {
+const navigate = useNavigate();
+  const handleClick = (name) => {
+    navigate(`/${name}`)
+  }
   return (
     <Stack direction={"row"} spacing={4} sx={{backgroundColor:'#fafafa'}}>
     <Stack spacing={0} direction="column" alignItems="flex-start" sx={{ width: 'fit-content', minWidth: '255px', cursor:'pointer' }}>
-      <ListItem variant="none" sx={{ width: '100%', borderRadius: '0px', borderColor:'white', fontSize: '1.2rem', height:'60px', border: "1px solid black", justifyContent:'center',}}><b>HARISH</b></ListItem>
-      <ListItem variant="outlined" sx={{ width: '100%', borderRadius: '0px', borderColor:'white', fontSize: '1.2rem', height:'60px', border: "1px solid black", "&:hover": {backgroundColor: "secondary.main", color: "#FFFFFF", fontWeight: 'bold'}}}><Stack direction={'row'} spacing={2} ><ImportantDevicesIcon /><span>Dashboard</span></Stack></ListItem>
-      <ListItem variant="outlined" sx={{ width: '100%', borderRadius: '0px', borderColor:'white', fontSize: '1.2rem', height:'60px', border: "1px solid black", "&:hover": {backgroundColor: "secondary.main", color: "#FFFFFF", fontWeight: 'bold'}}}><Stack direction={'row'} spacing={2}><ListAltIcon /><span>Purchased Test Series</span></Stack></ListItem>
-      <ListItem variant="outlined" sx={{ width: '100%', borderRadius: '0px', borderColor:'white', fontSize: '1.2rem', height:'60px',border: "1px solid black", "&:hover": {backgroundColor: "secondary.main", color: "#FFFFFF", fontWeight: 'bold'}}}><Stack direction={'row'} spacing={2}><FileCopyIcon /><span>Prelims Test Store</span></Stack></ListItem>
-      <ListItem variant="outlined" sx={{ width: '100%', borderRadius: '0px', borderColor:'white', fontSize: '1.2rem', height:'60px',border: "1px solid black", "&:hover": {backgroundColor: "secondary.main", color: "#FFFFFF", fontWeight: 'bold'}}}><Stack direction={'row'} spacing={2}><FileCopyIcon /><span>Mains Test Store</span></Stack></ListItem>
-      <ListItem variant="outlined" sx={{ width: '100%', borderRadius: '0px', borderColor:'white', fontSize: '1.2rem', height:'60px',border: "1px solid black", "&:hover": {backgroundColor: "secondary.main", color: "#FFFFFF", fontWeight: 'bold'}}}><Stack direction={'row'} spacing={2}><ScheduleIcon /> <span>Scheduled Test Store</span></Stack></ListItem>
-      <ListItem variant="outlined" sx={{ width: '100%', borderRadius: '0px', borderColor:'white', fontSize: '1.2rem', height:'60px',border: "1px solid black", "&:hover": {backgroundColor: "secondary.main", color: "#FFFFFF", fontWeight: 'bold'}}}><Stack direction={'row'} spacing={2}><ForumRoundedIcon /><span>Discussion</span></Stack></ListItem>
-      <ListItem variant="outlined" sx={{ width: '100%', borderRadius: '0px', borderColor:'white', fontSize: '1.2rem', height:'60px',border: "1px solid black", "&:hover": {backgroundColor: "secondary.main", color: "#FFFFFF", fontWeight: 'bold'}}}><Stack direction={'row'} spacing={2}><TrendingUpIcon /><span>Progress Card</span></Stack></ListItem>
-      <ListItem variant="outlined" sx={{ width: '100%', borderRadius: '0px', borderColor:'white', fontSize: '1.2rem', height:'60px',border: "1px solid black", "&:hover": {backgroundColor: "secondary.main", color: "#FFFFFF", fontWeight: 'bold'}}}><Stack direction={'row'} spacing={2}><ArchiveIcon /><span>Archives</span></Stack></ListItem>
-      <ListItem variant="outlined" sx={{ width: '100%', borderRadius: '0px', borderColor:'white', fontSize: '1.2rem', height:'60px',border: "1px solid black", "&:hover": {backgroundColor: "secondary.main", color: "#FFFFFF", fontWeight: 'bold'}}}><Stack direction={'row'} spacing={2}><SettingsIcon /><span>Settings</span></Stack></ListItem>
-      <ListItem sx={{paddingLeft:'20px',border: "1px solid black", width:'100%', borderbottom:'none'}}>#433, Second Floor<br/> 13th Main, M.C Layout<br/> Opp. Vijayanagar Post Office<br/> Vijayanagar<br/> Bangalore - 560040</ListItem>
+      <ListItem variant="none" sx={{ width: '100%', borderRadius: '0px', borderColor:'white', fontSize: '1.2rem', height:'60px', border: "1px solid black", justifyContent:'center',}} ><b>HARISH</b></ListItem>
+      <Customlistitem icon={<ImportantDevicesIcon />} name="Dashboard" onClick={() => handleClick("Dashboard")} />
+      <Customlistitem icon={<ListAltIcon />} name="Purchased Test Series" onClick={() => handleClick("Purchased Test Series")} />
+      <Customlistitem icon={<FileCopyIcon />} name="Prelims Test Store" onClick={() => handleClick("Prelims Test Store")} />
+      <Customlistitem icon={<FileCopyIcon />} name="Mains Test Store" onClick={() => handleClick("Mains Test Store")} />
+      <Customlistitem icon={<ScheduleIcon />} name="Scheduled Test Store" onClick={() => handleClick("Scheduled Test Store")} />
+      <Customlistitem icon={<ForumRoundedIcon />} name="Discussion" onClick={() => handleClick("Discussion")} />
+      <Customlistitem icon={<TrendingUpIcon />} name="Progress Card" onClick={() => handleClick("Progress Card")} />
+      <Customlistitem icon={<ArchiveIcon />} name="Archives" onClick={() => handleClick("Archives")} />
+      <Customlistitem icon={<SettingsIcon />} name="Settings" onClick={() => handleClick("Settings")} />
+     <ListItem sx={{paddingLeft:'20px',border: "1px solid black", width:'100%', borderbottom:'none'}}>#433, Second Floor<br/> 13th Main, M.C Layout<br/> Opp. Vijayanagar Post Office<br/> Vijayanagar<br/> Bangalore - 560040</ListItem>
     </Stack>
       <Welcomepage />
     </Stack>
