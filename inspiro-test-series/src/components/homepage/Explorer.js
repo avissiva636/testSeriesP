d85dea9b00da0c61b518ea2d7dd3918be1854bd3
Jagan26042000 <jagan26042000@gmail.com>
2024-02-12 00:15:46 +0530

    Removing space in navigate url name

--- inspiro-test-series/src/components/homepage/Explorer.js
+++ inspiro-test-series/src/components/homepage/Explorer.js
@@ -23,12 +23,12 @@ const navigate = useNavigate();
     <Stack spacing={0} direction="column" alignItems="flex-start" sx={{ width: 'fit-content', minWidth: '255px', cursor:'pointer' }}>
       <ListItem variant="none" sx={{ width: '100%', borderRadius: '0px', borderColor:'white', fontSize: '1.2rem', height:'60px', border: "1px solid black", justifyContent:'center',}} ><b>HARISH</b></ListItem>
       <Customlistitem icon={<ImportantDevicesIcon />} name="Dashboard" onClick={() => handleClick("Dashboard")} />
-      <Customlistitem icon={<ListAltIcon />} name="Purchased Test Series" onClick={() => handleClick("Purchased Test Series")} />
-      <Customlistitem icon={<FileCopyIcon />} name="Prelims Test Store" onClick={() => handleClick("Prelims Test Store")} />
-      <Customlistitem icon={<FileCopyIcon />} name="Mains Test Store" onClick={() => handleClick("Mains Test Store")} />
-      <Customlistitem icon={<ScheduleIcon />} name="Scheduled Test Store" onClick={() => handleClick("Scheduled Test Store")} />
+      <Customlistitem icon={<ListAltIcon />} name="Purchased Test Series" onClick={() => handleClick("PurchasedTestSeries")} />
+      <Customlistitem icon={<FileCopyIcon />} name="Prelims Test Store" onClick={() => handleClick("PrelimsTestStore")} />
+      <Customlistitem icon={<FileCopyIcon />} name="Mains Test Store" onClick={() => handleClick("MainsTestStore")} />
+      <Customlistitem icon={<ScheduleIcon />} name="Scheduled Test Store" onClick={() => handleClick("ScheduledTestStore")} />
       <Customlistitem icon={<ForumRoundedIcon />} name="Discussion" onClick={() => handleClick("Discussion")} />
-      <Customlistitem icon={<TrendingUpIcon />} name="Progress Card" onClick={() => handleClick("Progress Card")} />
+      <Customlistitem icon={<TrendingUpIcon />} name="Progress Card" onClick={() => handleClick("ProgressCard")} />
       <Customlistitem icon={<ArchiveIcon />} name="Archives" onClick={() => handleClick("Archives")} />
       <Customlistitem icon={<SettingsIcon />} name="Settings" onClick={() => handleClick("Settings")} />
      <ListItem sx={{paddingLeft:'20px',border: "1px solid black", width:'100%', borderbottom:'none'}}>#433, Second Floor<br/> 13th Main, M.C Layout<br/> Opp. Vijayanagar Post Office<br/> Vijayanagar<br/> Bangalore - 560040</ListItem>
