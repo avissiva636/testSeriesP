24781fe5c62e4eebc7abddca97d78120ba4d2792
Jagan26042000 <jagan26042000@gmail.com>
2024-02-11 23:17:43 +0530

    Home icon setup

--- inspiro-test-series/src/components/homepage/Navigationbar.js
+++ inspiro-test-series/src/components/homepage/Navigationbar.js
@@ -9,7 +9,10 @@ import {
   createTheme,
 } from "@mui/material";
 import NotificationsNoneOutlinedIcon from "@mui/icons-material/NotificationsNoneOutlined";
+import HomeOutlinedIcon from "@mui/icons-material/HomeOutlined";
+import { useNavigate } from "react-router-dom";
 const Navigationbar = () => {
+  const navigate = useNavigate();
   const theme = createTheme({
     palette: {
       white: {
@@ -18,32 +21,36 @@ const Navigationbar = () => {
     },
   });
   const notificationCount = 3;
+
   return (
     <Stack>
       <ThemeProvider theme={theme}>
         <AppBar position="fixed" color="white">
           <Toolbar>
-            <Stack spacing={69} direction={"row"}>
-              <Typography variant="h5">INSPIRO</Typography>
-              <Stack
-                sx={{
-                  padding: "4px",
-                  display: "inline-flex",
-                  borderRadius: "1px",
-                  cursor: "pointer",
-                  "&:hover": {
-                    border: "1px solid",
-                    color: "#FFFFFF",
-                  },
-                }}
-              >
-                <Badge badgeContent={4} color="error">
-                  <NotificationsNoneOutlinedIcon color="action" />
-                </Badge>
+            <Stack spacing={3} direction={"row"} onClick={()=>{navigate("/")}}>
+              <HomeOutlinedIcon />
+              <Stack spacing={69} direction={"row"}>
+                <Typography variant="h5">INSPIRO</Typography>
+                <Stack
+                  sx={{
+                    padding: "4px",
+                    display: "inline-flex",
+                    borderRadius: "1px",
+                    cursor: "pointer",
+                    "&:hover": {
+                      border: "1px solid",
+                      color: "#FFFFFF",
+                    },
+                  }}
+                >
+                  <Badge badgeContent={4} color="error">
+                    <NotificationsNoneOutlinedIcon color="action" />
+                  </Badge>
+                </Stack>
+                <Button color="success" variant="contained">
+                  Sign Out
+                </Button>
               </Stack>
-              <Button color="success" variant="contained">
-                Sign Out
-              </Button>
             </Stack>
           </Toolbar>
         </AppBar>
