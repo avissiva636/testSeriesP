8c9d607307e28487e7ba33b68f7a4f62b5dfa722
Jagan26042000 <jagan26042000@gmail.com>
2024-02-13 15:42:21 +0530

    Homepage fix

--- inspiro-test-series/src/components/homepage/Homepage.js
+++ inspiro-test-series/src/components/homepage/Homepage.js
@@ -1,15 +1,17 @@
 import { Stack } from "@mui/material";
 import Explorer from "./Explorer";
 import Navigationbar from "./Navigationbar";
+import Welcomepage from "./Welcomepage";
 
 const Homepage = () => {
   return (
-    
     <Stack direction="column" spacing={8}>
       <Navigationbar />
-      <Explorer />
-    
-  </Stack>
+      <Stack direction={"row"} spacing={4} sx={{ backgroundColor: "#fafafa" }}>
+        <Stack sx={{position:"fixed", overflow:"auto"}}><Explorer /></Stack>
+        <Welcomepage />
+      </Stack>
+    </Stack>
   );
 };
 export default Homepage;
