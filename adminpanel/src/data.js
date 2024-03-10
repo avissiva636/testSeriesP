6c1e986eda5454223055e6ed258e369bb08cea17
sivalingam <lingamsiva636@gmail.com>
2024-03-11 01:48:17 +0530

    sample data

--- adminpanel/src/data.js
+++ adminpanel/src/data.js
@@ -1,72 +1,83 @@
 export const subject = [
-    {
-      id: 1,
-      title: 'Lorem Ipsum',
-      description: 'Lorem ipsum dolor sit'
-    },
-    {
-      id: 2,
-      title: 'Nulla Consectetur',
-      description: 'Nulla consectetur justo at'
-    },
-    {
-      id: 3,
-      title: 'Fusce Egestas',
-      description: 'Fusce egestas ex non'
-    },
-    {
-      id: 4,
-      title: 'Pellentesque Tincidunt',
-      description: 'Pellentesque tincidunt ex a'
-    },
-    {
-      id: 5,
-      title: 'Vestibulum Tristique',
-      description: 'Vestibulum tristique nisl sit'
-    },
-    {
-      id: 6,
-      title: 'Aliquam Mollis',
-      description: 'Aliquam mollis purus a'
-    },
-    {
-      id: 7,
-      title: 'Donec Euismod',
-      description: 'Donec euismod ipsum nec'
-    },
-    {
-      id: 8,
-      title: 'Morbi Inceptos',
-      description: 'Morbi inceptos ipsum nec'
-    },
-    {
-      id: 9,
-      title: 'Suspendisse Cursus',
-      description: 'Suspendisse cursus magna non'
-    },
-    {
-      id: 10,
-      title: 'Phasellus Feugiat',
-      description: 'Phasellus feugiat magna non'
-    },
-    {
-      id: 11,
-      title: 'Integer Porta',
-      description: 'Integer porta velit eget'
-    },
-    {
-      id: 12,
-      title: 'Duis Consectetur',
-      description: 'Duis consectetur felis eget'
-    },
-    {
-      id: 13,
-      title: 'Etiam Tempor',
-      description: 'Etiam tempor quam in'
-    },
-    {
-      id: 14,
-      title: 'Mauris Fermentum',
-      description: 'Mauris fermentum leo vel'
-    }
-  ]
+  {
+    id: 1,
+    title: 'Lorem Ipsum',
+    description: 'Lorem ipsum dolor sit'
+  },
+  {
+    id: 2,
+    title: 'Nulla Consectetur',
+    description: 'Nulla consectetur justo at'
+  },
+  {
+    id: 3,
+    title: 'Fusce Egestas',
+    description: 'Fusce egestas ex non'
+  },
+  {
+    id: 4,
+    title: 'Pellentesque Tincidunt',
+    description: 'Pellentesque tincidunt ex a'
+  },
+  {
+    id: 5,
+    title: 'Vestibulum Tristique',
+    description: 'Vestibulum tristique nisl sit'
+  },
+  {
+    id: 6,
+    title: 'Aliquam Mollis',
+    description: 'Aliquam mollis purus a'
+  },
+  {
+    id: 7,
+    title: 'Donec Euismod',
+    description: 'Donec euismod ipsum nec'
+  },
+  {
+    id: 8,
+    title: 'Morbi Inceptos',
+    description: 'Morbi inceptos ipsum nec'
+  },
+  {
+    id: 9,
+    title: 'Suspendisse Cursus',
+    description: 'Suspendisse cursus magna non'
+  },
+  {
+    id: 10,
+    title: 'Phasellus Feugiat',
+    description: 'Phasellus feugiat magna non'
+  },
+  {
+    id: 11,
+    title: 'Integer Porta',
+    description: 'Integer porta velit eget'
+  },
+  {
+    id: 12,
+    title: 'Duis Consectetur',
+    description: 'Duis consectetur felis eget'
+  },
+  {
+    id: 13,
+    title: 'Etiam Tempor',
+    description: 'Etiam tempor quam in'
+  },
+  {
+    id: 14,
+    title: 'Mauris Fermentum',
+    description: 'Mauris fermentum leo vel'
+  }
+]
+
+
+export const Batch = [
+  { "id": "001", "course": "Mathematics", "batch": "Batch A", "description": "Calculus fundamentals" },
+  { "id": "002", "course": "History", "batch": "Batch B", "description": "World War II overview" },
+  { "id": "003", "course": "English Literature", "batch": "Batch C", "description": "Shakespearean plays analysis" },
+  { "id": "004", "course": "Computer Science", "batch": "Batch D", "description": "Introduction to programming" },
+  { "id": "005", "course": "Physics", "batch": "Batch E", "description": "Mechanics principles" },
+  { "id": "006", "course": "Biology", "batch": "Batch F", "description": "Cellular biology fundamentals" },
+  { "id": "007", "course": "Economics", "batch": "Batch G", "description": "Macroeconomic theory" }
+]
