2a9fe2ea5eef045ce99a7abc96aae4d339e1b1ee
sivalingam <lingamsiva636@gmail.com>
2024-03-11 17:03:36 +0530

    Add prelims

--- adminpanel/src/scenes/addprelimsseries/index.js
+++ adminpanel/src/scenes/addprelimsseries/index.js
@@ -1,8 +1,31 @@
-import React from 'react'
+import { Box } from '@mui/material'
+
+import Header from 'components/Header'
+import React, { useState } from 'react'
+import { useOutletContext } from 'react-router-dom';
+import AddPaperTemplate from 'components/AddPaperTemplate';
 
 const AddPrelimsSeries = () => {
+  const isNonMobile = useOutletContext();
+
+  const [batch, setBatch] = useState('');
+  const [batchDes, setBatchDes] = useState('');
+  const [price, setPrice] = useState('');
+  const [paymentLink, setPaymentLink] = useState('');
+
   return (
-    <div>AddPrelimsSeries</div>
+    <Box m="1.5rem 2.5rem" height={isNonMobile ? undefined : '80%'}>
+      <Header title="PRELIMS" subtitle="Add New Prelims" />
+
+      <AddPaperTemplate
+        isNonMobile={isNonMobile} seriesName="Prelims"
+        series={batch} setSeries={setBatch}
+        seriesDes={batchDes} setSeriesDes={setBatchDes}
+        seriesPrice={price} setSeriesPrice={setPrice}
+        seriesPaymentLink={paymentLink} setSeriesPaymentLink={setPaymentLink}
+      />
+
+    </Box>
   )
 }
 
