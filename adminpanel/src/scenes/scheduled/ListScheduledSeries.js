05b9756ea505b46d822ef5b13f417be7989acd41
sivalingam <lingamsiva636@gmail.com>
2024-03-12 02:16:56 +0530

    create ListScheduledSeries

---
+++ adminpanel/src/scenes/scheduled/ListScheduledSeries.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import { Box } from '@mui/material';
+import Header from 'components/Header';
+import ListPaperTemplate from 'components/ListPaperTemplate';
+import { PrelimsList as prelimsData } from 'data'
+import { useOutletContext } from 'react-router-dom';
+
+const ListScheduledSeries = () => {
+    const isNonMobile = useOutletContext();
+
+    return (
+        <Box m="1.5rem 2.5rem" height={isNonMobile ? undefined : '80%'}>
+            <Header title="SCHEDULED" subtitle="List Scheduled" />
+
+            <ListPaperTemplate paperData={prelimsData} />
+
+        </Box>
+    )
+}
+
+export default ListScheduledSeries
