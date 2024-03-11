c856d8a8fd1038f57be7cb558901881697649e23
sivalingam <lingamsiva636@gmail.com>
2024-03-12 01:37:22 +0530

    petty rm import

--- adminpanel/src/scenes/prelims/ListPrelimsSeries.js
+++ adminpanel/src/scenes/prelims/ListPrelimsSeries.js
@@ -1,7 +1,7 @@
 import { Box } from '@mui/material';
 import Header from 'components/Header';
 import ListPaperTemplate from 'components/ListPaperTemplate';
-import React, { useState } from 'react'
+import React from 'react'
 import { useOutletContext } from 'react-router-dom';
 import { PrelimsList as prelimsData } from 'data'
 
@@ -13,7 +13,7 @@ const ListPrelimsSeries = () => {
     <Box m="1.5rem 2.5rem" height={isNonMobile ? undefined : '80%'}>
       <Header title="PRELIMS" subtitle="List Prelims" />
 
-      <ListPaperTemplate paperData={prelimsData}/>
+      <ListPaperTemplate paperData={prelimsData} />
 
     </Box>
   )
