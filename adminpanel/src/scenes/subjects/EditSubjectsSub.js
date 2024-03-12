7229fe64728c69e2cf3241791f13e0b8ce25a5a8
sivalingam <lingamsiva636@gmail.com>
2024-03-12 14:35:25 +0530

    create EditSubjectsSub page

---
+++ adminpanel/src/scenes/subjects/EditSubjectsSub.js
@@ -0,0 +1,36 @@
+import { Box, useMediaQuery } from '@mui/material'
+import Header from 'components/Header'
+import TwoFieldEdit from 'components/TwoFieldEdit'
+import React, { useState } from 'react'
+import { useLocation, useSearchParams } from 'react-router-dom'
+
+const EditSubjectsSub = () => {
+    const [searchParams] = useSearchParams();
+
+    const id = searchParams.get('id');
+    const title = searchParams.get('title');
+    const description = searchParams.get('description');
+
+
+    console.log(title, description)
+
+    const [subject, setSubject] = useState(decodeURIComponent(title));
+    const [subjectDes, setSubjectDes] = useState(decodeURIComponent(description));
+    const isNonMediumScreens = useMediaQuery("(min-width: 1200px)");
+    console.log(useLocation().state)
+    return (
+        <Box m="1.5rem 2.5rem" height={isNonMediumScreens ? undefined : '80%'}>
+            <Header title="SUBJECT" subtitle="Add New Subject" />
+
+            <TwoFieldEdit
+                fieldName="Subject" fieldId={id}
+                field={subject} setfield={setSubject}
+                fieldDes={subjectDes} setFieldDes={setSubjectDes}
+                isNonMediumScreens={isNonMediumScreens}
+            />
+
+        </Box>
+    )
+}
+
+export default EditSubjectsSub
