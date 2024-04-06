0b312c44d2b991c5281afd1d56aad5588b49250b
sivalingam <lingamsiva636@gmail.com>
2024-04-06 22:22:08 +0530

    Backend connection

--- adminpanel/src/scenes/courses/AddCourses.js
+++ adminpanel/src/scenes/courses/AddCourses.js
@@ -2,12 +2,29 @@ import { Box, useMediaQuery } from '@mui/material'
 import Header from 'components/Header'
 import TwoFieldSubmition from 'components/TwoFieldSubmition'
 import React, { useState } from 'react'
+import { useCreateCourseMutation } from 'state/apiDevelopmentSlice'
 
 const AddCourses = () => {
     const [course, setCourse] = useState('');
     const [courseDes, setCourseDes] = useState('');
-
     const isNonMediumScreens = useMediaQuery("(min-width: 1200px)");
+
+    const [createCourse] = useCreateCourseMutation();
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
+        try {
+            await createCourse({ name: course, description: courseDes }).unwrap();
+            setCourse('');
+            setCourseDes('');
+
+        } catch (error) {
+            if (error.status === 400) {
+                alert("Give proper data");
+            }
+        }
+    }
 
     return (
         <Box m="1.5rem 2.5rem" height={isNonMediumScreens ? undefined : '80%'}>
@@ -18,6 +35,7 @@ const AddCourses = () => {
                 field={course} setfield={setCourse}
                 fieldDes={courseDes} setFieldDes={setCourseDes}
                 isNonMediumScreens={isNonMediumScreens}
+                handleSubmit={handleSubmit}
             />
 
         </Box>
