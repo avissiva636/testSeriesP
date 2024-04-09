1385bd9b9dccd88b837b5ab261700c953fac82e4
sivalingam <lingamsiva636@gmail.com>
2024-04-09 18:19:53 +0530

    indexing studetn model

--- server/src/database/index.js
+++ server/src/database/index.js
@@ -3,7 +3,8 @@ module.exports = {
     employeeModel: require("./models/Employee"),
     userModel: require("./models/User"),
     adminModel: require("./models/Admin"),
-    SubjecthModel:require("./models/Subject"),
-    CourseModel:require("./models/Course"),
-    BatchModel:require("./models/Batch"),
+    SubjecthModel: require("./models/Subject"),
+    CourseModel: require("./models/Course"),
+    BatchModel: require("./models/Batch"),
+    StudentModel: require("./models/Student"),
 };
