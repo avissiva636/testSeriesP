64245d0459234a7497ef19d480a44e35425607c0
sivalingam <lingamsiva636@gmail.com>
2024-05-15 12:53:33 +0530

    Add Back button in header

--- adminpanel/src/components/Header.jsx
+++ adminpanel/src/components/Header.jsx
@@ -1,17 +1,33 @@
-import { Box, Typography, useTheme } from '@mui/material'
+import { Box, Button, Typography, useTheme } from '@mui/material'
 import React from 'react'
+import FlexBetween from './FlexBetween';
+import { useNavigate } from 'react-router-dom';
 
-const Header = ({ title, subtitle }) => {
+const Header = ({ title, subtitle, isNavigate = false }) => {
     const theme = useTheme();
+    const navigate = useNavigate();
 
     return (
-        <Box>
-            <Typography
-                variant='h2'
-                color={theme.palette.secondary[100]}
-                fontWeight="bold"
-                sx={{ mb: "5px" }}
-            > {title} </Typography>
+        <Box maxWidth="1200px">
+            <FlexBetween>
+                <Typography
+                    variant='h2'
+                    color={theme.palette.secondary[100]}
+                    fontWeight="bold"
+                    sx={{ mb: "5px" }}
+                > {title} </Typography>
+
+                {isNavigate === true &&
+                    <Button
+                        component="label"
+                        variant="contained"
+                        onClick={() => navigate(-1)}
+                    >
+                        Back
+                    </Button>
+                }
+
+            </FlexBetween>
             <Typography variant='h5' color={theme.palette.secondary[300]} >
                 {subtitle}
             </Typography>
