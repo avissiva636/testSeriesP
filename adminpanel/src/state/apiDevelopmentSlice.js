af6b810c0f92d25e5728af65bd5ac2f17240c1d5
sivalingam <lingamsiva636@gmail.com>
2024-05-27 17:43:16 +0530

    implement auto refreshing access token

--- adminpanel/src/state/apiDevelopmentSlice.js
+++ adminpanel/src/state/apiDevelopmentSlice.js
@@ -1,10 +1,43 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+import { logOut, setCredentials } from "./stateSlice";
+
+const baseQuery = fetchBaseQuery({
+    baseUrl: process.env.REACT_APP_BASE_URL,
+    credentials: 'include',
+    prepareHeaders: (headers, { getState }) => {
+        const token = getState().auth.token;
+        if (token) {
+            headers.set("authorization", `Bearer ${token}`)
+        }
+        return headers
+    }
+})
+
+const baseQuesryWithReauth = async (args, api, extraOptions) => {
+    let result = await baseQuery(args, api, extraOptions);
+
+    if (result?.error?.originalStatus === 403) {
+        const refreshResult = await baseQuery({
+            url: '/admin/log/refresh',
+            method: 'POST'
+        }, api, extraOptions);
+        if (refreshResult?.data) {
+            const user = api.getState().auth.user;
+            // setting new action token
+            api.dispatch(setCredentials({ ...refreshResult.data, user }));
+
+            //retury with new access token
+            result = await baseQuery(args, api, extraOptions);
+        } else {
+            api.dispatch(logOut());
+        }
+    }
+
+    return result;
+}
 
 export const api = createApi({
-    baseQuery: fetchBaseQuery({
-        baseUrl: process.env.REACT_APP_BASE_URL,
-        credentials:'include'
-    }),
+    baseQuery: baseQuesryWithReauth,
     reducerPath: "adminApi",
     tagTypes: ["Subject", "Course", "Batch", "Student", "Outline",
         "Pseries", "Mseries",
