7734d936151e93db479fd9cd8140b830226d07d8
sivalingam <lingamsiva636@gmail.com>
2024-05-30 09:34:24 +0530

    1. server connection related variable name change 2.environment variable for url

--- inspiro-test-series/src/components/mainsTestSeries/Mainspage.js
+++ inspiro-test-series/src/components/mainsTestSeries/Mainspage.js
@@ -3,9 +3,12 @@ import Navigationbar from "../homepage/Navigationbar";
 import Explorer from "../homepage/Explorer";
 import { useInspiroCrud } from "../context/InspiroContext";
 import Appbar from "../homepage/Appbar";
+import { useGetMainsSeriesQuery } from "../../state/apiDevelopmentSlice";
 
 const Mainspage = () => {
   const { mainsTest } = useInspiroCrud();
+  const { isLoading: isMainsLoading, data: mainsData } = useGetMainsSeriesQuery({ userId: "66279432cc5ed3267265fd5d" });
+
   return (
     <Stack direction="row" spacing={8}>
       {/* <Navigationbar /> */}
@@ -20,74 +23,76 @@ const Mainspage = () => {
         justifyContent="space-evenly"
         flexWrap="wrap"
       >
-        {mainsTest.mains.map((mains) => (
-          <Card
-            variant="outlined"
-            sx={{
-              width: "400px",
-              minHeight: mainsTest.mains.length < 4 ? "260px" : "auto",
-              height: mainsTest.mains.length < 4 ? "200px" : "auto",
-              textAlign: "center",
-              margin: "10px",
-              borderColor: "ButtonShadow",
-              marginTop: "80px",
-            }}
-          >
-            <Grid direction={"column"} spacing={1}>
-              <Grid item>
-                <h2>{mains.title}</h2>
-                <p>{mains.description}</p>
-              </Grid>
-              {/* <Divider /> */}
-              <Stack
-                direction={"column"}
-                spacing={1}
-                sx={{ marginTop: "10px" }}
+        {
+          isMainsLoading ? <h1>Loading...</h1> :
+          mainsData?.mains.map((mains) => (
+              <Card
+                variant="outlined"
+                sx={{
+                  width: "400px",
+                  minHeight: mainsData?.mains.length < 4 ? "260px" : "auto",
+                  height: mainsData?.mains.length < 4 ? "200px" : "auto",
+                  textAlign: "center",
+                  margin: "10px",
+                  borderColor: "ButtonShadow",
+                  marginTop: "80px",
+                }}
               >
-                <Grid container justifyContent="center">
-                  <Grid
-                    item
-                    sx={{
-                      backgroundColor: "#f0f0f0",
-                      width: "300px",
-                      height: "35px",
-                      display: "flex",
-                      alignItems: "center",
-                      justifyContent: "center",
-                    }}
-                  >
-                    Purchase for ₹ <b>{mains.fee}</b>
+                <Grid direction={"column"} spacing={1}>
+                  <Grid item>
+                    <h2>{mains.mSeriesDetails.title}</h2>
+                    <p>{mains.mSeriesDetails.description}</p>
                   </Grid>
-                </Grid>
-                <Grid item>
+                  {/* <Divider /> */}
                   <Stack
-                    direction={"row"}
-                    spacing={2}
-                    sx={{
-                      padding: "10px 30px 0px 30px",
-                      justifyContent: "space-between",
-                      marginBottom: "10px",
-                    }}
+                    direction={"column"}
+                    spacing={1}
+                    sx={{ marginTop: "10px" }}
                   >
-                    <a href={mains.details}>
-                      <Button variant="contained" color="primary">
-                        Schedule
-                      </Button>
-                    </a>
-                    <h4 style={{ paddingTop: "10px" }}>
-                      {mains.testCount + " Tests"}
-                    </h4>
-                    <a href={mains.payHere}>
-                      <Button variant="contained" color="success">
-                        Buy
-                      </Button>
-                    </a>
+                    <Grid container justifyContent="center">
+                      <Grid
+                        item
+                        sx={{
+                          backgroundColor: "#f0f0f0",
+                          width: "300px",
+                          height: "35px",
+                          display: "flex",
+                          alignItems: "center",
+                          justifyContent: "center",
+                        }}
+                      >
+                        Purchase for ₹ <b>{mains.mSeriesDetails.price}</b>
+                      </Grid>
+                    </Grid>
+                    <Grid item>
+                      <Stack
+                        direction={"row"}
+                        spacing={2}
+                        sx={{
+                          padding: "10px 30px 0px 30px",
+                          justifyContent: "space-between",
+                          marginBottom: "10px",
+                        }}
+                      >
+                        <a href={`${process.env.REACT_APP_BASE_URL}/images/mains/${mains.mSeriesDetails.schedule}`}>
+                          <Button variant="contained" color="primary">
+                            Schedule
+                          </Button>
+                        </a>
+                        <h4 style={{ paddingTop: "10px" }}>
+                          {mains.totalCount + " Tests"}
+                        </h4>
+                        <a href={mains.mSeriesDetails.paymentLink}>
+                          <Button variant="contained" color="success">
+                            Buy
+                          </Button>
+                        </a>
+                      </Stack>
+                    </Grid>
                   </Stack>
                 </Grid>
-              </Stack>
-            </Grid>
-          </Card>
-        ))}
+              </Card>
+            ))}
       </Stack>
       {/* </Stack> */}
     </Stack>
