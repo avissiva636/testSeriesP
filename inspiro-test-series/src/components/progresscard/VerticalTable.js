a7e99616b2864c0158034ad066d8834ef52e3212
sivalingam <lingamsiva636@gmail.com>
2024-06-03 15:29:38 +0530

    variable name update

--- inspiro-test-series/src/components/progresscard/VerticalTable.js
+++ inspiro-test-series/src/components/progresscard/VerticalTable.js
@@ -5,29 +5,29 @@ import CloseIcon from '@mui/icons-material/Close';
 
 const VerticalTable = ({ data }) => {
   return (
-    <Stack sx={{margin:"0px 40px 0px 40px"}}>
-    <TableContainer sx={{ border: '1px solid rgba(0, 0, 0, 0.12)' }}>
-      <Table>
-        <TableHead>
-          <TableRow>
-            <TableCell>Q No</TableCell>
-            <TableCell>Result</TableCell>
-            <TableCell>Difficulty</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {data.map((row, index) => (
-            <TableRow key={index}>
-              <TableCell>{row.qno}</TableCell>
-              <TableCell>
-                {row.result === 'correct' ? <DoneIcon color="success" /> : <CloseIcon color="error" />}
-              </TableCell>
-              <TableCell>{row.difficulty}</TableCell>
+    <Stack sx={{ margin: "0px 40px 0px 40px" }}>
+      <TableContainer sx={{ border: '1px solid rgba(0, 0, 0, 0.12)' }}>
+        <Table>
+          <TableHead>
+            <TableRow>
+              <TableCell>Q No</TableCell>
+              <TableCell>Result</TableCell>
+              <TableCell>Difficulty</TableCell>
             </TableRow>
-          ))}
-        </TableBody>
-      </Table>
-    </TableContainer></Stack>
+          </TableHead>
+          <TableBody>
+            {data.map((row, index) => (
+              <TableRow key={index}>
+                <TableCell>{row.sno}</TableCell>
+                <TableCell>
+                  {row.status === 'correct' ? <DoneIcon color="success" /> : <CloseIcon color="error" />}
+                </TableCell>
+                <TableCell>{row.difficulty}</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer></Stack>
   );
 };
 
