bc41f16438ea9f85fce4f44f0942696c60268f75
sivalingam <lingamsiva636@gmail.com>
2024-06-03 15:31:09 +0530

    server connection related updates

--- inspiro-test-series/src/components/progresscard/ProgressCard.js
+++ inspiro-test-series/src/components/progresscard/ProgressCard.js
@@ -3,26 +3,34 @@ import Navigationbar from "../homepage/Navigationbar";
 import Explorer from "../homepage/Explorer";
 import PieChart from "./PieChart";
 import VerticalTable from "./VerticalTable";
-import { useNavigate } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
+import { useGetSpecificPrelimProgressQuery } from "../../state/apiDevelopmentSlice";
 
 const ProgressCard = () => {
   const navigate = useNavigate();
-    const data = {
-        totalQuestions: 50,
-        unAttended: 10,
-        questionsWrong: 10,
-        questionsCorrect: 30,
-        marks: 75,
-      };
-      const data1 = [
-        { qno: 1, result: 'correct', difficulty: 'easy' },
-        { qno: 2, result: 'wrong', difficulty: 'medium' },
-        { qno: 3, result: 'correct', difficulty: 'hard' },
-        // Add more data entries as needed
-      ];
-      const exitHandler = () => {
-        navigate("/")
-      }
+  const location = useLocation();
+  const { Progress } = location.state;
+  
+  const data = {
+    totalQuestions: Progress?.questionDescriptionId?.nQuestions,
+    unAttended: Progress?.questionDescriptionId?.nQuestions - Progress?.submitCount,
+    questionsWrong: Progress?.submitCount - Progress?.correctCount,
+    questionsCorrect: Progress?.correctCount,
+    marks: (Progress.correctCount * Progress.questionDescriptionId.cMarks) -
+      ((Progress.submitCount - Progress.correctCount) * Progress.questionDescriptionId.wMarks),
+  };
+
+  const { isLoading: isSPrelimLoading, data: sPrelimData } = useGetSpecificPrelimProgressQuery({ questionId: Progress?._id });
+  let data1 = null;
+  if (Progress?.result) {
+    data1 = Progress?.result;
+  }
+  else {
+    data1 = isSPrelimLoading ? [] : sPrelimData.foundResult.result;
+  }
+  const exitHandler = () => {
+    navigate("/")
+  }
   return (
     <Stack direction="column" spacing={8}>
       <Navigationbar />
@@ -34,51 +42,51 @@ const ProgressCard = () => {
           <Stack>
             <Card sx={{ height: "100vh", width: "88vh" }}>
               <Stack sx={{ alignItems: "center" }}>
-                <h1>Exam Title</h1>
+                <h1>Exam {Progress?.questionDescriptionId?.title}</h1>
               </Stack>
               <Stack spacing={2}>
-              <Stack sx={{margin:"0px 40px 0px 40px"}}>
-              <TableContainer component={Paper}>
-      <Table aria-label="simple table">
-        <TableBody>
-          <TableRow>
-            <TableCell component="th" scope="row">
-              Total Questions
-            </TableCell>
-            <TableCell align="right">{data.totalQuestions}</TableCell>
-          </TableRow>
-          <TableRow>
-            <TableCell component="th" scope="row">
-              Questions unattended
-            </TableCell>
-            <TableCell align="right">{data.unAttended}</TableCell>
-          </TableRow>
-          <TableRow>
-            <TableCell component="th" scope="row">
-              Questions Wrong
-            </TableCell>
-            <TableCell align="right">{data.questionsWrong}</TableCell>
-          </TableRow>
-          <TableRow>
-            <TableCell component="th" scope="row">
-              Questions Correct
-            </TableCell>
-            <TableCell align="right">{data.questionsCorrect}</TableCell>
-          </TableRow>
-          <TableRow>
-            <TableCell component="th" scope="row">
-              Marks
-            </TableCell>
-            <TableCell align="right">{data.marks}</TableCell>
-          </TableRow>
-        </TableBody>
-      </Table>
-    </TableContainer>
-              </Stack>
-              <Stack>
-                <VerticalTable data={data1}/>
-              </Stack>
-              <Stack sx={{alignItems:"center"}} ><Button color="success" variant="contained" sx={{width:"10px"}} onClick={exitHandler}>Exit</Button></Stack>
+                <Stack sx={{ margin: "0px 40px 0px 40px" }}>
+                  <TableContainer component={Paper}>
+                    <Table aria-label="simple table">
+                      <TableBody>
+                        <TableRow>
+                          <TableCell component="th" scope="row">
+                            Total Questions
+                          </TableCell>
+                          <TableCell align="right">{data.totalQuestions}</TableCell>
+                        </TableRow>
+                        <TableRow>
+                          <TableCell component="th" scope="row">
+                            Questions unattended
+                          </TableCell>
+                          <TableCell align="right">{data.unAttended}</TableCell>
+                        </TableRow>
+                        <TableRow>
+                          <TableCell component="th" scope="row">
+                            Questions Wrong
+                          </TableCell>
+                          <TableCell align="right">{data.questionsWrong}</TableCell>
+                        </TableRow>
+                        <TableRow>
+                          <TableCell component="th" scope="row">
+                            Questions Correct
+                          </TableCell>
+                          <TableCell align="right">{data.questionsCorrect}</TableCell>
+                        </TableRow>
+                        <TableRow>
+                          <TableCell component="th" scope="row">
+                            Marks
+                          </TableCell>
+                          <TableCell align="right">{data.marks}</TableCell>
+                        </TableRow>
+                      </TableBody>
+                    </Table>
+                  </TableContainer>
+                </Stack>
+                <Stack>
+                  <VerticalTable data={data1} />
+                </Stack>
+                <Stack sx={{ alignItems: "center" }} ><Button color="success" variant="contained" sx={{ width: "10px" }} onClick={exitHandler}>Exit</Button></Stack>
               </Stack>
             </Card>
           </Stack>
