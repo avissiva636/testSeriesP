0ca72d350d252eb2433c1a7267df40d99d99f3d5
sivalingam <lingamsiva636@gmail.com>
2024-06-03 15:18:52 +0530

    1. user prelims Route added, updated 2. controller user logic created, updated

--- server/src/routes/api/user/userPrelims.js
+++ server/src/routes/api/user/userPrelims.js
@@ -2,8 +2,9 @@ const express = require('express');
 const router = express.Router();
 const uPrelimsController = require('../../../controllers/user/uPrelimsController');
 
-// /user/prelims
 router.route('/:uid').get(uPrelimsController.getPrelimsPapers);
+
+router.route('/prelimAttempt/:uid').get(uPrelimsController.getPrelimAttempt);
 
 router.route('/exam/:qno')
     .get(uPrelimsController.getPrelimsPaper)
