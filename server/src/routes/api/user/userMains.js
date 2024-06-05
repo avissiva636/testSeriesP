553fb8923d15435d453d56ef337db13729da0325
sivalingam <lingamsiva636@gmail.com>
2024-06-05 21:49:38 +0530

    1.uMainsUpload 2.create new route

--- server/src/routes/api/user/userMains.js
+++ server/src/routes/api/user/userMains.js
@@ -1,15 +1,15 @@
 const express = require('express');
 const router = express.Router();
 const uMainsController = require('../../../controllers/user/uMainsController');
-
+const { uMainsUpload } = require("../../../util/middleware/uImageUpload")
 // /user/prelims
 router.route('/:uid').get(uMainsController.getMainsPapers);
 
-router.route('/exam/:qno')
+router.route('/mainsAttempt/:uid').get(uMainsController.getMainsAttempt);
+
+router.route('/exam')
     .get(uMainsController.getMainsPaper)
-    .post(uMainsController.submitMainsPaper);
-
-
+    .post(uMainsUpload.single('mainsAnswer'), uMainsController.submitMainsPaper);
 
 
 module.exports = router;
